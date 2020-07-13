a554e45d740d7a1fd4777950d03891ee8e4645c7
ShoSakashita <kakel1993@gmail.com>
2020-07-13 22:18:38 +0900

    ラベル、ボタン作成

---
+++ AIBO_Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AIBO_Controller
+//
+//  Created by ShoSakashita on 2020/07/13.
+//  Copyright © 2020 ShoSakashita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
